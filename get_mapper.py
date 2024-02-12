57377b7adfe283faff81b4f569453f8240cc632e
Davide Stenner <davide.stenner@cefriel.com>
2024-02-12 21:58:35 +0100

    get mapper utilities

---
+++ get_mapper.py
@@ -0,0 +1,10 @@
+if __name__=='__main__':
+    from contextlib import redirect_stdout
+    from src.utils.import_utils import import_config
+    from src.utils.dtype import get_mapping_info
+    
+    config = import_config()
+    with open('log/log_mapper.txt', 'w') as f:
+        with redirect_stdout(f):
+            get_mapping_info(config=config)
+    
