0b71ef1e86fc242f7f9151a44005b6877c470549
Davide Stenner <davide.stenner@cefriel.com>
2024-02-12 21:58:18 +0100

    update init

--- src/base/initialize.py
+++ src/base/initialize.py
@@ -1,6 +1,3 @@
-import polars as pl
-
-from typing import Union, Any
 from abc import ABC, abstractmethod
 
 class BaseInit(ABC):
@@ -8,6 +5,3 @@ class BaseInit(ABC):
     @abstractmethod
     def _initialiaze_empty_dataset(self) -> None:
         pass
-    
-    def _collect_item_utils(self, data: Union[pl.DataFrame, pl.LazyFrame]) -> Any:
-        return data.item() if self.inference else data.collect().item()
