abd20020b2ebd903e01850ae41ffc874e2bf8170
Davide Stenner <davide.stenner@cefriel.com>
2024-03-15 00:22:24 +0100

    encapsulate every method

--- test_package/test_pipeline.py
+++ test_package/test_pipeline.py
@@ -10,58 +10,78 @@ class TestPipeline(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         warnings.filterwarnings("ignore", category=UserWarning)
-
-        self.config_dict: Dict[str, Any] = import_config()
        
         _, experiment_name = import_params(model='lgb')
         self.experiment_name: str = experiment_name
         
-        self.pipeline_data: PreprocessPipeline = PreprocessPipeline(
-            config_dict=self.config_dict, 
-            embarko_skip=6
-        )
-        
-        self.trainer: LgbmPipeline = LgbmPipeline(
-            experiment_name=experiment_name + "_lgb",
-            params_lgb={},
-            config_dict=self.config_dict, 
-            data_columns=self.pipeline_data.feature_list,
-            metric_eval='gini_stability', log_evaluation=50 
-        )
-
     def test_preprocess_activate_inference(self):
         try:
-            self.pipeline_data.begin_inference()
+            config = import_config()
+            pipeline_data: PreprocessPipeline = PreprocessPipeline(
+                config_dict=config, 
+                embarko_skip=6
+            )
+            
+            pipeline_data.begin_inference()
         except Exception as e:
             self.fail(e)
     
     def test_preprocess_activate_inference(self):
         try:
-            self.trainer.activate_inference()
+            config = import_config()
+            pipeline_data: PreprocessPipeline = PreprocessPipeline(
+                config_dict=config, 
+                embarko_skip=6
+            )
+            
+            trainer: LgbmPipeline = LgbmPipeline(
+                experiment_name=self.experiment_name + "_lgb",
+                params_lgb={},
+                config_dict=config, 
+                data_columns=pipeline_data.feature_list,
+                metric_eval='gini_stability', log_evaluation=50 
+            )
+
+
+            trainer.activate_inference()
         except Exception as e:
             self.fail(e)
 
     def test_preprocess_import(self):
         try:
-            self.pipeline_data.test_all_import(n_rows=1000)
+            
+            config = import_config()
+            pipeline_data: PreprocessPipeline = PreprocessPipeline(
+                config_dict=config, 
+                embarko_skip=6
+            )
+            
+            pipeline_data.test_all_import(n_rows=1000)
+            
         except Exception as e:
             self.fail(e)
 
     def test_preprocess_pipeline(self):
         try:
-            self.pipeline_data.test_all()
+            config = import_config()
+            pipeline_data: PreprocessPipeline = PreprocessPipeline(
+                config_dict=config, 
+                embarko_skip=6
+            )
+            
+            pipeline_data.test_all()
         except Exception as e:
             self.fail(e)
     
     def test_blank_dataset_pipeline(self):
-        config_dict = self.config_dict
+        config_dict = import_config()
         config_dict['PATH_ORIGINAL_DATA'] = os.path.join(
             config_dict['PATH_TESTING_DATA'],
             'empty_dataset',
         )
 
         pipeline_data = PreprocessPipeline(
-            config_dict=self.config_dict, 
+            config_dict=config_dict, 
             embarko_skip=6
         )
         try:
@@ -70,19 +90,21 @@ class TestPipeline(unittest.TestCase):
             self.fail(e)
     
     def test_random_dataset_pipeline(self):
-        config_dict = self.config_dict
+        config_dict = import_config()
         config_dict['PATH_ORIGINAL_DATA'] = os.path.join(
             config_dict['PATH_TESTING_DATA'],
             'random_dataset',
         )
 
         pipeline_data = PreprocessPipeline(
-            config_dict=self.config_dict, 
+            config_dict=config_dict, 
             embarko_skip=6
         )
         try:
             pipeline_data.test_all()
         except Exception as e:
+            self.fail(e)
+    
     def test_pipeline_on_test_data(self):
         try:
             config = import_config()
