2de9c8497e97acc23ad9a4df924322f38c237a95
Davide Stenner <davide.stenner@cefriel.com>
2024-03-27 14:25:52 +0100

    fastest way to import file

--- src/script/eda_line_stability.py
+++ src/script/eda_line_stability.py
@@ -5,6 +5,7 @@ sys.path.append(os.getcwd())
 def get_line_stability() -> None:
 
     import warnings
+    import polars as pl
     
     from src.utils.import_utils import import_config
     from src.preprocess.pipeline import PreprocessPipeline
@@ -17,19 +18,18 @@ def get_line_stability() -> None:
     if not os.path.isdir('eda/line_stability'):
         os.makedirs('eda/line_stability')
 
-    home_credit_preprocessor = PreprocessPipeline(
-        config_dict=config_dict, 
-        embarko_skip=6
+    dataset = pl.read_parquet(
+        os.path.join(
+            config_dict['PATH_PARQUET_DATA'],
+            'data.parquet'
+        )
     )
-    #use everything    
-    home_credit_preprocessor.import_all()
-    home_credit_preprocessor.create_feature()
-    home_credit_preprocessor.merge_all()
-    home_credit_preprocessor.add_additional_feature()
-
-    dataset = home_credit_preprocessor.data.collect()
-
-    for dataset_name in home_credit_preprocessor.used_dataset:
+    used_dataset = (
+        config_dict['DEPTH_0'] + 
+        config_dict['DEPTH_1'] + 
+        config_dict['DEPTH_2']
+    )
+    for dataset_name in used_dataset:
         try:
             print('Running', dataset_name)
             save_multiple_line_plot(dataset=dataset, dataset_name=dataset_name, save_path='eda/line_stability')
