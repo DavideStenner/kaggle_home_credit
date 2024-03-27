4160cdcdfe8867d95c30ac2065029b00415d5b38
Davide Stenner <davide.stenner@cefriel.com>
2024-03-27 22:06:17 +0100

    null stabilty eda

--- src/script/eda_line_stability.py
+++ src/script/eda_line_stability.py
@@ -18,21 +18,38 @@ def get_line_stability() -> None:
     if not os.path.isdir('eda/line_stability'):
         os.makedirs('eda/line_stability')
 
+    if not os.path.isdir('eda/null_stability'):
+        os.makedirs('eda/null_stability')
+
     dataset = pl.read_parquet(
         os.path.join(
             config_dict['PATH_PARQUET_DATA'],
             'data.parquet'
         )
     )
+    dataset_is_null = dataset.with_columns(
+        pl.col(
+            [col for col in dataset.columns if col != 'date_decision']
+        ).is_null().cast(pl.UInt8)
+    )
     used_dataset = (
         config_dict['DEPTH_0'] + 
         config_dict['DEPTH_1'] + 
         config_dict['DEPTH_2']
     )
-    for dataset_name in used_dataset:
+    for dataset_name in used_dataset + ['tax_registry_1']:
         try:
             print('Running', dataset_name)
-            save_multiple_line_plot(dataset=dataset, dataset_name=dataset_name, save_path='eda/line_stability')
+            save_multiple_line_plot(
+                dataset=dataset, 
+                dataset_name=dataset_name, 
+                save_path='eda/line_stability'
+            )
+            save_multiple_line_plot(
+                dataset=dataset_is_null, 
+                dataset_name=dataset_name, 
+                save_path='eda/null_stability'
+            )
             print('Finished', dataset_name)
         except Exception as e:
             print(dataset_name, 'failed...\n\n')
