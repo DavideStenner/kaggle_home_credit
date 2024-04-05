2e8aa81c6faecd653fd3f4eeb827227a749b32a5
Davide Stenner <davide.stenner@cefriel.com>
2024-04-05 13:02:08 +0200

    add type to title plot

--- src/eda/stability_feature.py
+++ src/eda/stability_feature.py
@@ -10,14 +10,16 @@ from matplotlib.backends.backend_pdf import PdfPages
 warnings.filterwarnings(action='ignore', category=FutureWarning)
 plt.ioff()
 
-def line_plot(data: pl.DataFrame, col: str) -> plt.figure:
+def line_plot(data: pl.DataFrame, col: str, dtype_col: str) -> plt.figure:
+    suffix: str = '  ' + dtype_col
+    
     if data.select(pl.col(col).is_null().mean()).item() == 1.:
         data = data.with_columns(
             pl.lit(0).alias(col)
         )
-        suffix: str = '  is all empty'
+        suffix  += '  is all empty'
     else:
-        suffix: str = ''
+        pass
         
     plt.figure(figsize=(12,8))
     fig = sns.lineplot(
@@ -36,6 +38,18 @@ def save_multiple_line_plot(dataset: pl.DataFrame, dataset_name: str, save_path:
 
     predictor_ = [col for col in dataset.columns if dataset_name in col]
     
+    dataset_selected = dataset.select(predictor_)
+
+    dtype_list = [
+        str(pl_dtype)
+        for pl_dtype in dataset_selected.dtypes
+    ]
+
+    mapping_col_dtype = {
+        predictor_[i]: dtype_list[i]
+        for i in range(dataset_selected.width)
+    }
+    
     data_selected = dataset.group_by('date_decision').agg(
         pl.col(col).mean()
         for col in predictor_
@@ -44,6 +58,6 @@ def save_multiple_line_plot(dataset: pl.DataFrame, dataset_name: str, save_path:
     with PdfPages(save_path_file) as export_pdf:
         
         for col in predictor_:
-            fig = line_plot(data_selected, col=col)
+            fig = line_plot(data_selected, col=col, dtype_col=mapping_col_dtype[col])
             export_pdf.savefig()  # saves the current figure into a pdf page
             plt.close()
